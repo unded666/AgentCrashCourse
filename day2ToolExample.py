f95f8c378e5d4e6535dc922fe6c9cbb5459e5cee
Evan Hurwitz <evan.hurwitz@avast.com>
2025-11-11 16:48:30 +0000

    added days content

---
+++ day2ToolExample.py
@@ -0,0 +1,24 @@
+from google import genai
+from google.genai.types import Tool, GenerateContentConfig, HttpOptions, UrlContext
+
+client = genai.Client(http_options=HttpOptions(api_version="v1"))
+model_id = "gemini-2.5-flash"
+
+url_context_tool = Tool(url_context = UrlContext)
+url1 = "https://www.foodnetwork.com/recipes/ina-garten/perfect-roast-chicken-recipe-1940592"
+url2 = "https://www.allrecipes.com/recipe/70679/simple-whole-roasted-chicken/"
+
+response = client.models.generate_content(
+    model=model_id,
+    contents=("Compare the ingredients and cooking times from "f"the recipes at {url1} and {url2}"),
+    config=GenerateContentConfig(
+        tools=[url_context_tool],
+        response_modalities=["TEXT"],
+    )
+)
+
+for each in response.candidates[0].content.parts:
+    print(each.text)
+# For verification, you can inspect the metadata to see which URLs the
+# model retrieved
+print(response.candidates[0].url_context_metadata)
--- printkey.py
+++ printkey.py
@@ -2,3 +2,7 @@ import os
 
 key = os.environ.get('GEMINI_API_KEY')
 print(f'GEMINI_API_KEY: {key}')
+key2 = os.environ.get('GEMINI_PLAYGROUND_KEY')
+print(f'GEMINI_PLAYGROUND_KEY: {key2}')
+key3 = os.environ.get('CHATGPT_KEY')
+print(f'CHATGPT_KEY: {key3}')
