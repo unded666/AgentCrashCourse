c6d43430cf1c4a66e5cae7d73b8c59fc50134d21
Evan Hurwitz <evan.hurwitz@avast.com>
2025-11-13 16:55:11 +0000

    completed day 4

---
+++ Notebooks/research-agent/agent.py
@@ -0,0 +1,52 @@
+
+from google.adk.agents import LlmAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.tools.agent_tool import AgentTool
+from google.adk.tools.google_search_tool import google_search
+
+from google.genai import types
+from typing import List
+
+retry_config = types.HttpRetryOptions(
+    attempts=5,  # Maximum retry attempts
+    exp_base=7,  # Delay multiplier
+    initial_delay=1,
+    http_status_codes=[429, 500, 503, 504],  # Retry on these HTTP errors
+)
+
+# ---- Intentionally pass incorrect datatype - `str` instead of `List[str]` ----
+def count_papers(papers: List[str]):
+    """
+    This function counts the number of papers in a list of strings.
+    Args:
+      papers: A list of strings, where each string is a research paper.
+    Returns:
+      The number of papers in the list.
+    """
+    return len(papers)
+
+
+# Google Search agent
+google_search_agent = LlmAgent(
+    name="google_search_agent",
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    description="Searches for information using Google search",
+    instruction="""Use the google_search tool to find information on the given topic. Return the raw search results.
+    If the user asks for a list of papers, then give them the list of research papers you found and not the summary.""",
+    tools=[google_search]
+)
+
+
+# Root agent
+root_agent = LlmAgent(
+    name="research_paper_finder_agent",
+    model=Gemini(model="gemini-2.5-flash-lite", retry_options=retry_config),
+    instruction="""Your task is to find research papers and count them. 
+
+    You MUST ALWAYS follow these steps:
+    1) Find research papers on the user provided topic using the 'google_search_agent'. 
+    2) Then, pass the papers to 'count_papers' tool to count the number of papers returned.
+    3) Return both the list of research papers and the total number of papers.
+    """,
+    tools=[AgentTool(agent=google_search_agent), count_papers]
+)
